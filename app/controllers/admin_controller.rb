96fbcb392192044fbf49aa087984a56ca81ed8f6
Vinay K Kaimal <vinaykaimal44@gmail.com>
2011-09-12 07:35:27 +0530

    Added Button to find the number of times two people have paired

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,31 @@
 class AdminController < ApplicationController
-  before_filter :create_person, :only => :home
-  def create_person
-    @person
-    ThotWorker.create(:name => "Vinay")
-    ThotWorker.create(:name => "Ash")
-    ThotWorker.create(:name => "Piyush")
-  end
+  #before_filter :create_person, :only => :home
+  #def create_person
+  #  @person
+  #  ThotWorker.create(:name => "Vinay")
+  #  ThotWorker.create(:name => "Ash")
+  #  ThotWorker.create(:name => "Piyush")
+  #end
   def home
     @person =Person.create
   end
 
   def matrix
     @person = Person.create(params[:person])
-    tw1 = ThotWorker.find_by_name(@person.name1)
-    tw2 = ThotWorker.find_by_name(@person.name1)
-
-      ThotWorker.pair(ThotWorker.find_by_name(@person.name1),ThotWorker.find_by_name(@person.name2))
-        @index
+    ThotWorker.pair(ThotWorker.find_by_name(@person.name1),ThotWorker.find_by_name(@person.name2))
+    @index
   end
+
+  def relation
+    @person =Person.create
+  end
+
+  def result
+    @person = Person.create(params[:person])
+    @index = ThotWorker.times_paired(ThotWorker.find_by_name(@person.name1).id,ThotWorker.find_by_name(@person.name2).id)
+
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PairingMatrix::Application.routes.draw do
   root :to => "admin#home"
   match "matrix" => "admin#matrix"
+  match "relation" => "admin#relation"
+  match "result" => "admin#result"
 
 end
